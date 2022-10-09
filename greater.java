c7606784afa352f993253bdd5914c3f9d9456fb5
Quratulainsoomro <115370825+Quratulainsoomro@users.noreply.github.com>
2022-10-09 14:18:22 +0500

    Add files via upload

---
+++ greater.java
@@ -0,0 +1,13 @@
+class greater{
+public static void main(String[] args){
+int a = 10;
+int b = 11;
+System.out.println(a<=b);
+if (a<b) {
+    System.out.println("a is < b");
+}
+else{
+    System.out.println("a is > b");
+}
+}
+} 
